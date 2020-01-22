19278593751eaeb845a9b3b1d7ea471409dd61c4
agral <adam@gralin.ski>
2020-01-22 21:35:30 +0100

    Add several methods to CairoHelper

--- Utils/CairoHelper.lua
+++ Utils/CairoHelper.lua
@@ -1,4 +1,5 @@
 require("cairo")
+local Color = require("Color")
 
 local CairoHelper = {
 }
@@ -7,6 +8,17 @@ setmetatable(CairoHelper, {
   __index = CairoHelper,
 } )
 
+function CairoHelper:ImageSurfaceCreateFromPng(path)
+  return cairo_image_surface_create_from_png(path)
+end
+
+function CairoHelper:ImageSurfaceGetHeight(image)
+  return cairo_image_surface_get_height(image)
+end
+
+function CairoHelper:ImageSurfaceGetWidth(image)
+  return cairo_image_surface_get_width(image)
+end
 
 function CairoHelper:LineTo(x, y)
   cairo_line_to(cr, x, y)
@@ -14,6 +26,27 @@ end
 
 function CairoHelper:MoveTo(x, y)
   cairo_move_to(cr, x, y)
+end
+
+function CairoHelper:Paint()
+  cairo_paint(cr)
+end
+
+function CairoHelper:Restore()
+  cairo_restore(cr)
+end
+
+function CairoHelper:Save()
+  cairo_save(cr)
+end
+
+function CairoHelper:Scale(sx, sy)
+  cairo_scale(cr, sx, sy)
+end
+
+function CairoHelper:SetColor(color)
+  assert(type(color) == "table", "[CairoHelper] SetColor(): Malformed color")
+  self:SetSourceRgba(color.r, color.g, color.b, color.a)
 end
 
 function CairoHelper:SetLineWidth(newLineWidth)
@@ -24,16 +57,16 @@ function CairoHelper:SetSourceRgba(red, green, blue, alpha)
   cairo_set_source_rgba(cr, red, green, blue, alpha)
 end
 
+function CairoHelper:SetSourceSurface(surface, x, y)
+  cairo_set_source_surface(cr, surface, x, y)
+end
+
 function CairoHelper:Stroke()
   cairo_stroke(cr)
 end
 
-function CairoHelper:DrawDebugLine()
-  self:SetLineWidth(10)
-  self:SetSourceRgba(1.0, 0.0, 1.0, 0.5)
-  self:MoveTo(50, 50)
-  self:LineTo(100, 100)
-  self:Stroke()
+function CairoHelper:SurfaceDestroy(surface)
+  cairo_surface_destroy(surface)
 end
 
 return CairoHelper
