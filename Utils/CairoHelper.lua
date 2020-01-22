be1f140738c2ed75f623b28e4c1403e732eb6027
agral <adam@gralin.ski>
2020-01-23 00:50:53 +0100

    Extend CairoHelper's text handling utilities

--- Utils/CairoHelper.lua
+++ Utils/CairoHelper.lua
@@ -1,12 +1,37 @@
 require("cairo")
-local Color = require("Color")
 
 local CairoHelper = {
+  Font = {
+    Slant = {
+      Normal = CAIRO_FONT_SLANT_NORMAL,
+      Italic = CAIRO_FONT_SLANT_ITALIC,
+      Oblique = CAIRO_FONT_SLANT_OBLIQUE,
+    },
+    Weight = {
+      Normal = CAIRO_FONT_WEIGHT_NORMAL,
+      Bold = CAIRO_FONT_WEIGHT_BOLD,
+    },
+  },
+  LineCap = {
+    Round = CAIRO_LINE_CAP_ROUND,
+    Butt = CAIRO_LINE_CAP_BUTT,
+  },
 }
 
 setmetatable(CairoHelper, {
   __index = CairoHelper,
 } )
+
+function CairoHelper:Fill()
+  cairo_fill(cr)
+end
+
+function CairoHelper:GetTextSize(text)
+  local extents = cairo_text_extents_t:create()
+  tolua.takeownership(extents)
+  cairo_text_extents(cr, text, extents)
+  return extents.width, extents.height
+end
 
 function CairoHelper:ImageSurfaceCreateFromPng(path)
   return cairo_image_surface_create_from_png(path)
@@ -49,8 +74,26 @@ function CairoHelper:SetColor(color)
   self:SetSourceRgba(color.r, color.g, color.b, color.a)
 end
 
+function CairoHelper:SelectFontFace(name, slant, weight)
+  slant = slant or self.Font.Slant.Normal
+  weight = weight or self.Font.Weight.Normal
+  cairo_select_font_face(cr, name, slant, weight)
+end
+
+function CairoHelper:SetFontSize(newFontSize)
+  cairo_set_font_size(cr, newFontSize)
+end
+
+function CairoHelper:SetLineCap(newLineCap)
+  cairo_set_line_cap(cr, newLineCap)
+end
+
 function CairoHelper:SetLineWidth(newLineWidth)
   cairo_set_line_width(cr, newLineWidth)
+end
+
+function CairoHelper:ShowText(text)
+  cairo_show_text(cr, text)
 end
 
 function CairoHelper:SetSourceRgba(red, green, blue, alpha)
