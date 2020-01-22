935e1c25718736dfcbc75a83caec652336f0e64a
agral <adam@gralin.ski>
2020-01-22 13:05:20 +0100

    Rewrite CairoHelper's methods as internals

--- Utils/CairoHelper.lua
+++ Utils/CairoHelper.lua
@@ -7,12 +7,33 @@ setmetatable(CairoHelper, {
   __index = CairoHelper,
 } )
 
-function CairoHelper:DrawDebugLine()
-  cairo_set_line_width(cr, 10)
-  cairo_set_source_rgba(cr, 1.0, 0.0, 1.0, 0.5)
-  cairo_move_to(cr, 50, 50)
-  cairo_line_to(cr, 100, 100)
+
+function CairoHelper:LineTo(x, y)
+  cairo_line_to(cr, x, y)
+end
+
+function CairoHelper:MoveTo(x, y)
+  cairo_move_to(cr, x, y)
+end
+
+function CairoHelper:SetLineWidth(newLineWidth)
+  cairo_set_line_width(cr, newLineWidth)
+end
+
+function CairoHelper:SetSourceRgba(red, green, blue, alpha)
+  cairo_set_source_rgba(cr, red, green, blue, alpha)
+end
+
+function CairoHelper:Stroke()
   cairo_stroke(cr)
+end
+
+function CairoHelper:DrawDebugLine()
+  self:SetLineWidth(10)
+  self:SetSourceRgba(1.0, 0.0, 1.0, 0.5)
+  self:MoveTo(50, 50)
+  self:LineTo(100, 100)
+  self:Stroke()
 end
 
 return CairoHelper
