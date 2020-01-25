b5afeaa2bb98d7c9ffe3f0219a36db48c80f1967
agral <adam@gralin.ski>
2020-01-25 14:34:25 +0100

    Draw analog clock hands and bars in Clock module

--- Modules/Clock.lua
+++ Modules/Clock.lua
@@ -22,9 +22,44 @@ Clock.Analog.mark = {
     cap = CAIRO_LINE_CAP_ROUND,
     color = Solarized.BASE00,
     length = 5,
-    width = 2
+    width = 2,
   },
-},
+}
+Clock.Analog.Hands = {
+  hour = {
+    cap = CAIRO_LINE_CAP_BUTT,
+    color = Solarized.BLUE:WithAlpha(0.95),
+    radius = {
+      inner = Clock.Analog.radius - 40,
+      outer = Clock.Analog.radius - 15,
+    },
+    width = 6,
+  },
+  minute = {
+    cap = CAIRO_LINE_CAP_BUTT,
+    color = Solarized.BLUE:WithAlpha(0.75),
+    radius = {
+      inner = Clock.Analog.radius - 40,
+      outer = Clock.Analog.radius - 5,
+    },
+    width = 3,
+  },
+  second = {
+    bar = {
+      cap = CAIRO_LINE_CAP_BUTT,
+      color = Solarized.MAGENTA:WithAlpha(0.4),
+      radius = Clock.Analog.radius - 2,
+      width = 4,
+    },
+    cap = CAIRO_LINE_CAP_ROUND,
+    color = Solarized.MAGENTA:WithAlpha(0.8),
+    radius = {
+      inner = Clock.Analog.radius - 8,
+      outer = Clock.Analog.radius - 2,
+    },
+    width = 3,
+  },
+}
 setmetatable(Clock, {__index = Clock,})
 
 function Clock:Draw(params, settingsOverride)
@@ -32,10 +67,12 @@ function Clock:Draw(params, settingsOverride)
   self.cairo, self.x, self.y = params.cairo, params.x, params.y
   -- TODO: add merge-tables utility, allow overriding of default settings.
 
+  self:GetDateTime()
   self:DrawAnalogClock()
 end
 
 function Clock:DrawAnalogClock()
+  -- Draws the clock face: a round border and twelve hour marks:
   self.cairo:SetColor(self.Analog.border.color)
   self.cairo:SetLineWidth(self.Analog.border.width)
   self.cairo:Arc(self.x, self.y, self.Analog.radius, 0, 2 * math.pi)
@@ -56,6 +93,46 @@ function Clock:DrawAnalogClock()
     self.cairo:LineTo(x, y)
     self.cairo:Stroke()
   end
+
+  -- Draws the clock hands:
+  local angles = {
+    hour = math.pi * self.Time.InSeconds.hours / 21600,
+    minute = math.pi * self.Time.InSeconds.minutes / 1800,
+    second = math.pi * self.Time.seconds / 30
+  }
+  for name, hand in pairs(self.Analog.Hands) do
+    self.cairo:SetColor(hand.color)
+    self.cairo:SetLineCap(hand.cap)
+    self.cairo:SetLineWidth(hand.width)
+    x = self.x + hand.radius.outer * math.sin(angles[name])
+    y = self.y - hand.radius.outer * math.cos(angles[name])
+    self.cairo:MoveTo(x, y)
+    x = self.x + hand.radius.inner * math.sin(angles[name])
+    y = self.y - hand.radius.inner * math.cos(angles[name])
+    self.cairo:LineTo(x, y)
+    self.cairo:Stroke()
+    if hand.bar then
+      local startAngle, endAngle = -0.5 * math.pi, math.pi * (-0.5 + (self.Time.seconds / 30))
+      self.cairo:SetColor(hand.bar.color)
+      self.cairo:SetLineCap(hand.bar.cap)
+      self.cairo:SetLineWidth(hand.bar.width)
+      self.cairo:Arc(self.x, self.y, hand.bar.radius, startAngle, endAngle)
+      self.cairo:Stroke()
+    end
+  end
+end
+
+function Clock:GetDateTime()
+  local time = os.date("*t")
+  self.Time = {
+    seconds = time.sec,
+    minutes = time.min,
+    hours = time.hour,
+    InSeconds = {
+      minutes = 60 * time.min + time.sec,
+    },
+  }
+  self.Time.InSeconds.hours = self.Time.InSeconds.minutes + 3600 * self.Time.hours
 end
 
 return Clock
